e48d1f59db3296bdfc3f1a021cce37aa8b0f392b
dilekbusra <busrakandamar@outlook.com>
2020-12-25 01:48:19 +0300

    Added try catch

--- src/lib/service.js
+++ src/lib/service.js
@@ -2,10 +2,15 @@ import axios from "axios"
 const ENDPOINT = "https://jsonplaceholder.typicode.com";
 
 async function  getData(userId){
-	const { data: users } = await axios.get(`${ENDPOINT}/users/${userId}`);
+try{
+    const { data: users } = await axios.get(`${ENDPOINT}/users/${userId}`);
     const { data: posts } = await axios.get(`${ENDPOINT}/posts?userId=${userId}`);
     
     return {busra: users, posts}
+}
+catch(err){
+    console.log(err);
+}
 }
 
 export default getData; 
